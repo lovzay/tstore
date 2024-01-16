f8afb417332fe6c5d52d7a4ef53a77d5ab56c377
lovzay <tfvin24@gmail.com>
2024-01-16 18:50:38 +0800

    created text_strings.dart, renamed from texts.dart

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,19 @@
+/// this clas contains all the app text in string formats
+
+class TTexts {
+
+  // global texts
+
+  // onboarding texts
+  static const String onboardingTitle1 = 'Choose your product';
+  static const String onboardingTitle2 = 'Select Payment Method';
+  static const String onboardingTitle3 = 'Deliver at your doorstep';
+
+  static const String onboardingSubtitle1 = 'Welcome to a World of Limitless Choices - Your Perfect Product Awaits!';
+  static const String onboardingSubtitle2 = 'For Seamless Transactions, Choose Your Payment Method - Your Convenience, Our Priority!';
+  static const String onboardingSubtitle3 = 'From Our Doorstep To Yours - Swift, Secure, and Contactless Delivery!';
+
+  // Home
+  static const String homeAppbarTitle = 'Good day for shopping';
+  static const String homeAppbarSubtitle = 'What are you looking for?';  
+}
