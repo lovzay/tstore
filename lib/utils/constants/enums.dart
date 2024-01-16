1edc02799b3a20a437ab21e8379a7508fed17b80
lovzay <tfvin24@gmail.com>
2024-01-16 18:50:02 +0800

    created enums.dart

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,19 @@
+/// list of enums
+/// lthey cannot be created inside a class
+///
+
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods {
+  paypal,
+  googlePlay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  paystack,
+  razorPay,
+  paytm
+}
