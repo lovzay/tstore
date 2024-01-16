0c942e622aab70fa1995c81e6c26561bee464a30
lovzay <tfvin24@gmail.com>
2024-01-16 18:50:13 +0800

    created image_strings

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,12 @@
+/// this class contains all the app images in string formats
+class TImages {
+
+  // app logos
+  static const String darkAppLogo = 'assets/logos/t-store-splash-logo-black.png';
+  static const String whiteAppLogo = 'assets/logos/t-store-splash-logo-white.png';
+
+  // social logos
+  static const String google = 'assets/logos/google-icon.png';
+  static const String facebook = 'assets/logos/facebook-icon.png';
+  
+}
